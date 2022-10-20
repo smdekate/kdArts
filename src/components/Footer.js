870212d483c222e920bee3419a764e6e5f5f4c39
smdekate <smdekate@gmail.com>
2022-10-20 15:45:51 +0530

    create basic layout

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <div className="container-fluid footer bg-secondary">
+            <h1>This is footer</h1>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Main() {
+    return (
+        <div className="container">
+            <h1>This is main section</h1>
+        </div>
+    )
+}
+
+export default Main;
